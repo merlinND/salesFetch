4db8a332dc148d54066f77d59c5c74ac45b9aefe
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-01-28 16:59:24 +0100

    Dummy users

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ var mongoose = require('mongoose');
 var config = require('./config');
 
 mongoose.connect(config.mongo_url);
-
 var app = express();
-
 
 // Views settings
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Logger configuration
+app.use(express.logger('dev'));
+
+// serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('users', {
+    users: users,
+    title: "EJS example",
+    header: "Some users"
+  });
 });
 
 module.exports = app;
