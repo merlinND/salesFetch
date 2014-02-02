ee6f1505abf583274a8eede6ee15341dbbf64090
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-02 17:24:28 +0100

    Add organisation

---
+++ models/organization.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orgModel = new Schema ({
+  organizationId: {type: String, required: true, unique: true},
+  name: String,
+  currency: String
+});
+
+module.exports = mongoose.model('Organization', orgModel);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,27 @@ var crypto = require("crypto");
 var config = require('../config');
 var User = require('../models/user');
 
+var authenticateUser = function(context, cb) {
+  User.findOne({userId: context.userId}, function(err, user) {
+    if (err) {
+      return cb(err);
+    }
+
+    if (user) {
+      cb(null, user);
+    } else {
+      user = new User({
+        name: context.fullName,
+        userId: context.userId,
+        email: context.email
+      });
+
+      user.save(cb);
+    }
+
+  });
+};
+
 exports.index = function(req, res) {
   if (req.user) {
     res.send("On the index and connected with " + req.user.name);
@@ -22,25 +43,18 @@ exports.authenticate = function(req, res) {
   if (check === consumerSecret) {
     var envelope = JSON.parse(new Buffer(encodedEnvelope, "base64"));
 
-    var contextUser = envelope.context.user;
-
-    User.findOne({userId: contextUser.userId}, function(err, user) {
+    authenticateUser(envelope.context.user, function(err, user) {
       if (err) {
-        return res.send(500, { error: 'something blew up' });
-      }
-
-      if (!user) {
-        console.log("New user connection");
-        user = new User({
-          name: contextUser.fullName,
-          userId: contextUser.userId,
-          email: contextUser.email
-        }).save();
+        return res.send(500, {error: 'Problem during retrieving user'});
       }
 
       req.user = user;
-      exports.index(req, res);
+      req.context = envelope.context;
+
+      res.send(200, user);
     });
+
+
   } else {
     res.send(401);
   }
