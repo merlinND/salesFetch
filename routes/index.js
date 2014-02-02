fd4126959a4e4c8e720d84f452e696a4d5851817
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-02 10:55:49 +0100

    display if connected or not

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var crypto = require("crypto");
 var config = require('../config');
 
 exports.index = function(req, res) {
-  res.send(200);
+  if (req.user) {
+    res.send("On the index and connected with " + req.user.fullName);
+  } else {
+    res.send("On the index");
+  }
 };
 
 exports.authenticate = function(req, res) {
@@ -12,11 +16,14 @@ exports.authenticate = function(req, res) {
   var consumerSecret = bodyArray[0];
   var encodedEnvelope = bodyArray[1];
 
+  // Check the request validity
   var check = crypto.createHmac("sha256", config.consumer_secret).update(encodedEnvelope).digest("base64");
   if (check === consumerSecret) {
     var envelope = JSON.parse(new Buffer(encodedEnvelope, "base64"));
     req.user = envelope.context.user;
     console.log(envelope);
+
+    exports.index(req, res);
   } else {
     res.send(401);
   }
