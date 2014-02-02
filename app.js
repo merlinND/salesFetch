0f55f991e909217e4b030da8622fdc8b18c1cd71
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-02 10:37:45 +0100

    Add secret and consumer keys for post request

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ var express = require('express');
 var https = require('https');
 var mongoose = require('mongoose');
 var fs = require('fs');
-
 var config = require('./config');
 var express_configuration = require('./config/express');
 var routes = require('./routes');
 
-mongoose.connect(config.mongo_url);
-
-// Create and configure the express application
 var options = {
   key: fs.readFileSync('ssl-key.pem'),
   cert: fs.readFileSync('ssl-cert.pem')
 };
 
+mongoose.connect(config.mongo_url);
+
+// Create and configure the express application
 var app = express();
 express_configuration(app);
 
--- config/index.js
+++ config/index.js
@@ -15,9 +15,18 @@ var root_path = path.normalize(__dirname + '/..');
 // mongo url connection
 var default_mongo_url = "mongodb://localhost/" + node_env;
 
+// Salesfetch configuration
+if (!process.env.CONSUMER_KEY || !process.env.CONSUMER_SECRET) {
+  console.log('[ERROR] CONSUMER_KEY or CONSUMER_SECRET is missing.');
+  process.exit(1);
+}
+
 module.exports = {
   env: node_env,
   root_path: root_path,
   port: process.env.PORT || default_port,
-  mongo_url: process.env.MONGO_URL || default_mongo_url
+  mongo_url: process.env.MONGO_URL || default_mongo_url,
+
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET
 };
