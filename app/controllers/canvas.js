705b9eeea8144faa5d0593839f5282b14c92f08b
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-04 07:01:18 +0100

    Add params for searching

--- app/controllers/canvas.js
+++ app/controllers/canvas.js
@@ -3,8 +3,24 @@
  */
 'use strict';
 
-module.exports = function(req, res) {
+var displaySearch = function(req, res) {
   res.render('canvas/timeline.ect', {
     user: req.user
   });
+};
+
+var displayContext = function(req, res) {
+  res.render('canvas/timeline.ect', {
+    user: req.user
+  });
+};
+
+module.exports = function(req, res) {
+  var params = req.context.environment.parameters;
+
+  if(params.indexOf("search") !== -1) {
+    displaySearch(req, res);
+  } else {
+    displayContext(req, res);
+  }
 };
