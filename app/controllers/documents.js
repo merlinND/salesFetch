d6a365af69f18e0f956268e2806defd521c9fa5d
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-12 07:10:16 +0100

    Formatting of the resquest return for frontend

--- app/controllers/documents.js
+++ app/controllers/documents.js
@@ -14,7 +14,7 @@ var anyFetchRequest = function(url, params) {
       'Authorization': 'Basic ' + process.env.FETCHAPI_CREDS
     }
   };
-}
+};
 
 module.exports.index = function(req, res) {
   var json = {};
@@ -46,13 +46,23 @@ module.exports.index = function(req, res) {
 
     var docReturn = JSON.parse(data[1]);
     var rootReturn = JSON.parse(data[0]);
+    json = docReturn;
+
+    json.document_types = {};
     for(var doc_type in rootReturn.document_types) {
       if(docReturn.document_types[doc_type]) {
-        rootReturn.document_types[doc_type].documents = docReturn.document_types[doc_type].documents;
+        json.document_types[doc_type] = rootReturn.document_types[doc_type];
+        json.document_types[doc_type].documents = docReturn.document_types[doc_type].documents;
       }
-    };
-    // json.document_types = data[0].document_types;
-    // json.providers = data[0].provider_status;
+    }
+
+    json.providers = {};
+    for(var provider in rootReturn.provider_status) {
+      if(docReturn.tokens[provider]) {
+        json.providers[provider] = rootReturn.provider_status[provider];
+        json.providers[provider].documents = docReturn.tokens[provider];
+      }
+    }
 
     res.send(json);
   });
