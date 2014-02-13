8bead2cbfb84b974db39a061c56c312d4770bd8d
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-13 18:23:30 +0100

    Fix the return element

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,31 @@
 'use strict';
 
+var viewer = null;
+var isViewer = window.opener ? true : false;
+
+var displayFull = function(url) {
+  if (!viewer) {
+    viewer = window.open(url,"_blank","toolbar=yes, scrollbars=yes, resizable=yes, width=800, height=1000");
+    var interval = window.setInterval(function() {
+        try {
+          if (viewer === null || viewer.closed) {
+            window.clearInterval(interval);
+            viewer = null;
+          }
+        }
+        catch (e) {
+        }
+      }, 200);
+  } else {
+    viewer.location = url;
+    viewer.focus();
+  }
+};
+
 $( ".snippet-container a" ).click(function(e) {
-  e.preventDefault();
-  window.open($(this)[0].href,"_blank","toolbar=yes, scrollbars=yes, resizable=yes, fullscreen=yes");
+  if (!isViewer) {
+    e.preventDefault();
+
+    displayFull($(this)[0].href);
+  }
 });
