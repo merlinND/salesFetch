244a68f19ab68067f5f99755350c60ac019e033c
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-13 14:51:39 +0100

    Render results on the server

--- app/controllers/canvas.js
+++ app/controllers/canvas.js
@@ -3,6 +3,9 @@
  */
 'use strict';
 
+var async = require('async');
+var request = require('request');
+var Mustache = require('mustache');
 
 /*
  * Display Search page
@@ -13,14 +16,73 @@ var displaySearch = function(req, res) {
   });
 };
 
+var anyFetchRequest = function(url, params) {
+  var translatedParameters = {};
+
+  if (params && params.query) {
+    translatedParameters.search = params.query;
+  }
+
+  return {
+    url: url,
+    qs: translatedParameters,
+    headers: {
+      'Authorization': 'Basic ' + process.env.FETCHAPI_CREDS
+    }
+  };
+};
+
+var retrieveDocuments = function(context, cb) {
+  async.parallel([
+    function(cb) {
+      request(anyFetchRequest('http://api.anyfetch.com'), function(err, resp, body) {
+        if (err) {
+          return cb(err, null);
+        }
+
+        cb(null, body);
+      });
+    },
+    function(cb){
+      request(anyFetchRequest('http://api.anyfetch.com/documents', context), function(err, resp, body) {
+        if (err) {
+          return cb(err, null);
+        }
+
+        cb(null, body);
+      });
+    }
+  ], function(err, data){
+    if (err) {
+      return cb(err);
+    }
+
+    var docReturn = JSON.parse(data[1]);
+    var rootReturn = JSON.parse(data[0]);
+
+    docReturn.datas.forEach(function(doc) {
+      var realatedTemplate = rootReturn.document_types[doc.document_type].template_snippet;
+      doc.snippet_rendered = Mustache.render(realatedTemplate, doc.datas);
+    });
+
+    cb(null, docReturn);
+  });
+};
+
+
 /*
  * Display specific context
  */
 var displayContext = function(req, res) {
   var params = req.session.context.environment.parameters;
-  res.render('canvas/timeline.html', {
-    creds: process.env.FETCHAPI_CREDS,
-    context: params.record
+  retrieveDocuments(params.record, function(err, datas) {
+
+    console.log(datas.datas);
+    res.render('canvas/timeline.html', {
+      context: params.record,
+      documents: datas
+    });
+
   });
 };
 
