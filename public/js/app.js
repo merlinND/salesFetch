41b19cae2961167f95d0eb1bbd5733e42676212c
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-13 17:12:25 +0100

    Open full in a new window

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,6 @@
 'use strict';
 
-angular.module('sFetch', ['ngCookies', 'ngResource', 'ngRoute', 'sFetch.system']);
-
-angular.module('sFetch.system', []);
-
-//Setting HTML5 Location Mode
-angular.module('sFetch').config(['$sceProvider',function($sceProvider) {
-
-  // Disable templating HTML protection for Mustache
-  $sceProvider.enabled(false);
-
-}]);
+$( ".snippet-container a" ).click(function(e) {
+  e.preventDefault();
+  window.open($(this)[0].href,"_blank","toolbar=yes, scrollbars=yes, resizable=yes, fullscreen=yes");
+});
