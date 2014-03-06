e3405fb2dd61aaeb03ceae3f40154997d349db90
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-03-06 12:31:05 +0100

    Display the context

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -4,15 +4,33 @@
 'use strict';
 
 var async = require('async');
+var jsforce = require('jsforce');
+var _ = require('lodash');
 var mongoose = require('mongoose'),
     Organization = mongoose.model('Organization');
+
+var retrieveSObject = function(passedContext, cb) {
+  // Retrive the context object
+  var conn = new jsforce.Connection({
+    instanceUrl : passedContext.instance_url,
+    accessToken : passedContext.oauth_token
+  });
+  conn.describeGlobal(function(err, data) {
+    if (err) {
+      return cb(err);
+    }
+
+    var availableObjects = _.where(data.sobjects, {'layoutable': true, 'activateable': false});
+    cb(null, availableObjects);
+  });
+};
 
 module.exports.index = function(req, res) {
   Organization.findOne({_id: req.session.user.organization}, function(err, org) {
     if (err || !org || !org.context_profilers) {
       return res.send(500);
     }
-
+    console.log(req.session.context);
     res.render('admin/index.html', {
       profilers: org.context_profilers
     });
@@ -20,7 +38,11 @@ module.exports.index = function(req, res) {
 };
 
 module.exports.newContextProfiler = function(req, res) {
-  res.render('admin/new.html');
+  retrieveSObject(req.session.context, function(err, objects) {
+    res.render('admin/new.html', {
+      objects: objects
+    });
+  });
 };
 
 module.exports.createContextProfiler = function(req, res) {
