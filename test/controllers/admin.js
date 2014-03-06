ae8915593e8f6e24edf7394e21285dd6cea3643f
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-03-06 19:37:20 +0100

    Testing for context-profiler creation

--- test/controllers/admin.js
+++ test/controllers/admin.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var request = require('supertest');
+var async = require('async');
+
 var app = require('../../app.js');
 var login = require('../helpers/login');
 var APIs = require('../helpers/APIs');
@@ -56,6 +58,77 @@ describe('<Admin controller>', function() {
           options.should.have.length(2);
         })
         .end(done);
+    });
+  });
+
+  describe('/admin/context-profiler/new', function() {
+    var endPoint = '/admin/context-profiler/';
+
+    it('should accept well formated request', function(done) {
+      async.series([
+        function(cb) {
+          var req = request(app)
+            .post(endPoint,
+            {
+              record_type: 'Case',
+              query_template: '{{Name}}',
+              display_template: '{{Name}}'
+            });
+          agent.attachCookies(req);
+
+          req
+            .expect(302)
+            .end(cb);
+        },
+        function(cb) {
+          var req = request(app).get('/admin');
+          agent.attachCookies(req);
+          req
+            .expect(200)
+            .expect(function(res) {
+              var options = res.text.match(/Targeted context/g);
+              options.should.have.length(4);
+            })
+            .end(cb);
+        }
+      ], done);
+    });
+
+    it('should reject if duplicate object type', function(done) {
+      var req = request(app)
+        .post(endPoint,
+        {
+          record_type: 'Contact',
+          query_template: '{{Name}}',
+          display_template: '{{Name}}'
+        });
+      agent.attachCookies(req);
+
+      req
+        .expect(/New Context Profiler/)
+        .expect(/alert-danger/)
+        .end(done);
+    });
+
+    it('should reject if a missing field', function(done) {
+      var keys = ['record_type', 'query_template', 'display_template'];
+
+      async.each(keys, function(key, cb) {
+        var contextProfiler = {
+          record_type: 'Contact',
+          query_template: '{{Name}}',
+          display_template: '{{Name}}'
+        };
+        delete contextProfiler[key];
+        var req = request(app)
+          .post(endPoint, contextProfiler);
+        agent.attachCookies(req);
+
+        req
+          .expect(/New Context Profiler/)
+          .expect(/alert-danger/)
+          .end(cb);
+      }, done);
     });
   });
 
