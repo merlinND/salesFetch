ff6dd912799d54b467421d1577df96706f85b1b1
Neamar <neamar@neamar.fr>
2014-03-31 11:31:43 +0200

    Fix indentation

--- app/helpers/anyfetch.js
+++ app/helpers/anyfetch.js
@@ -5,100 +5,100 @@ var Mustache = require('mustache');
 
 
 var baseRequest = function(url, endpoint, cb) {
-	var urlToCall = url + endpoint;
-	return request.get(urlToCall)
-		.set('Authorization', 'Basic ' + process.env.FETCHAPI_CREDS)
-		.end(function(e, r) {cb(e,r);});
+  var urlToCall = url + endpoint;
+  return request.get(urlToCall)
+    .set('Authorization', 'Basic ' + process.env.FETCHAPI_CREDS)
+    .end(function(e, r) {cb(e,r);});
 };
 
 
 module.exports.findDocuments = function(url, params, cb) {
-	var query = [];
-	for(var key in params) {
-		query.push(key + "=" + encodeURIComponent(params[key]));
-	}
+  var query = [];
+  for(var key in params) {
+    query.push(key + "=" + encodeURIComponent(params[key]));
+  }
 
-	var pages = [
-		'/document_types',
-		'/providers',
-		'/documents?' + query.join('&')
-	];
-
-
-	var batchParams = pages.map(encodeURIComponent).join('&pages=');
-	baseRequest(url, '/batch?pages=' + batchParams, function(err, res) {
-
-		if (err) {
-			return cb(err);
-		}
-
-		var body = res.body;
-
-		var documentTypes = body[pages[0]];
-		var providers = body[pages[1]];
-		var docReturn = body[pages[2]];
+  var pages = [
+    '/document_types',
+    '/providers',
+    '/documents?' + query.join('&')
+  ];
 
 
-		if (!docReturn.datas) {
-			return cb(null, docReturn);
-		}
+  var batchParams = pages.map(encodeURIComponent).join('&pages=');
+  baseRequest(url, '/batch?pages=' + batchParams, function(err, res) {
 
-		// Render the datas templated
-		docReturn.datas.forEach(function(doc) {
-			var relatedTemplate = documentTypes[doc.document_type].template_snippet;
-			doc.snippet_rendered = Mustache.render(relatedTemplate, doc.datas);
+    if (err) {
+      return cb(err);
+    }
 
-			doc.provider = providers[doc.token].name;
-			doc.document_type = documentTypes[doc.document_type].name;
-		});
+    var body = res.body;
 
-		// Return all the documents types
-		for (var docType in docReturn.document_types) {
-			if (docType) {
-				docReturn.document_types[docType] = documentTypes[docType];
-			}
-		}
+    var documentTypes = body[pages[0]];
+    var providers = body[pages[1]];
+    var docReturn = body[pages[2]];
 
-		// Return all the providers
-		docReturn.providers = {};
-		for (var provider in docReturn.tokens) {
-			if (provider) {
-				docReturn.providers[provider] = providers[provider];
-			}
-		}
 
-		cb(null, docReturn);
-	});
+    if (!docReturn.datas) {
+      return cb(null, docReturn);
+    }
+
+    // Render the datas templated
+    docReturn.datas.forEach(function(doc) {
+      var relatedTemplate = documentTypes[doc.document_type].template_snippet;
+      doc.snippet_rendered = Mustache.render(relatedTemplate, doc.datas);
+
+      doc.provider = providers[doc.token].name;
+      doc.document_type = documentTypes[doc.document_type].name;
+    });
+
+    // Return all the documents types
+    for (var docType in docReturn.document_types) {
+      if (docType) {
+        docReturn.document_types[docType] = documentTypes[docType];
+      }
+    }
+
+    // Return all the providers
+    docReturn.providers = {};
+    for (var provider in docReturn.tokens) {
+      if (provider) {
+        docReturn.providers[provider] = providers[provider];
+      }
+    }
+
+    cb(null, docReturn);
+  });
 };
 
 module.exports.findDocument = function(url, id, cb) {
-	var pages = [
-		'/document_types',
-		'/providers',
-		'/documents/' + id
-	];
+  var pages = [
+    '/document_types',
+    '/providers',
+    '/documents/' + id
+  ];
 
-	var batchParams = pages.map(encodeURIComponent).join('&pages=');
-	baseRequest(url, '/batch?pages=' + batchParams, function(err, res) {
-		if (err) {
-			return cb(err);
-		}
+  var batchParams = pages.map(encodeURIComponent).join('&pages=');
+  baseRequest(url, '/batch?pages=' + batchParams, function(err, res) {
+    if (err) {
+      return cb(err);
+    }
 
-		var body = res.body;
+    var body = res.body;
 
-		var documentTypes = body[pages[0]];
-		var providers = body[pages[1]];
-		var docReturn = body[pages[2]];
+    var documentTypes = body[pages[0]];
+    var providers = body[pages[1]];
+    var docReturn = body[pages[2]];
 
-		var relatedTemplate = documentTypes[docReturn.document_type].template_full;
-		var titleTemplate = documentTypes[docReturn.document_type].template_title;
+    var relatedTemplate = documentTypes[docReturn.document_type].template_full;
+    var titleTemplate = documentTypes[docReturn.document_type].template_title;
 
-		docReturn.full_rendered = Mustache.render(relatedTemplate, docReturn.datas);
-		docReturn.title_rendered = Mustache.render(titleTemplate, docReturn.datas);
+    docReturn.full_rendered = Mustache.render(relatedTemplate, docReturn.datas);
+    docReturn.title_rendered = Mustache.render(titleTemplate, docReturn.datas);
 
-		docReturn.provider = providers[docReturn.token].name;
-		docReturn.document_type = documentTypes[docReturn.document_type].name;
+    docReturn.provider = providers[docReturn.token].name;
+    docReturn.document_type = documentTypes[docReturn.document_type].name;
 
-		cb(null, docReturn);
-	});
+    cb(null, docReturn);
+  });
 };
