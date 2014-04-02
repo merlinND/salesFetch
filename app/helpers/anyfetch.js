46d9b5cb3f7f18410f9027d4d8e21d97049c4c2e
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-04-02 17:47:03 +0200

    Delete useless console.log

--- app/helpers/anyfetch.js
+++ app/helpers/anyfetch.js
@@ -38,8 +38,6 @@ module.exports.findDocuments = function(url, params, cb) {
     var providers = body[pages[1]];
     var docReturn = body[pages[2]];
 
-    console.log(res.body);
-
     if (!docReturn.datas) {
       return cb(null, docReturn);
     }
